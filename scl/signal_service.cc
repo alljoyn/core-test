d19fe4e1925a36c6b5ce5f25b18912826327889a
Karol Matuszkiewicz <kmatuszkiewicz@affinegy.com>
2016-07-29 11:19:39 +0200

    ASACORE-2748 Deprecate description members related to Introspect...
    Calls to old accessors replaced with calls to the new ones.
    
    Change-Id: I15a532a0829ace63f89ed18e8ee25251cffb7bf3
    Signed-off-by: Karol Matuszkiewicz <kmatuszkiewicz@affinegy.com>

--- scl/signal_service.cc
+++ scl/signal_service.cc
@@ -68,28 +68,6 @@ static void CDECL_CALL SigIntHandler(int sig)
 static const char* tags[] = { "en", "de", "hi" };
 static const char* objId = "obj";
 static const char* objDescription[] =  { "This is the object", "Dies ist das Objekt", "Ye Object hai" };
-static const char* ifcId = "ifc";
-static const char* ifcDescription[] =  { "This is the first interface", "Dies ist das UNO Schnittstelle", "Ye pehla Interface hai" };
-static const char* ifcIdTest = "ifcTest";
-static const char* ifcDescriptionTest[] =  { "This is the second interface", "Dies ist das second Schnittstelle", "Ye dusra Interface hai" };
-static const char* nameChangedId = "nameChanged";
-static const char* nameChangedDescription[] =  { "Emitted when the name changes", "Emittiert, wenn der Name andert", "Naam badalne pe emitte karen" };
-static const char* nameChangedIdTest = "nameChangedTest";
-static const char* nameChangedDescriptionTest[] =  { "Emitted when the name changes and is sessionless", "Emittiert, wenn der Name andert sessionless", "Naam badalne pe emitte karen aur ye sessionless hai" };
-static const char* testMethodId = "testMethod";
-static const char* testMethodDescription[] = { "This is first method", "Este es el primer metodo", "Ye pehla method hai" };
-static const char* nameChangedId1 = "nameChanged1";
-static const char* nameChangedDescription1[] =  { "Emitted when the name changes 2nd interface", "Emittiert, wenn der Name andert 2nd interface", "Naam badalne pe emitte karen 2nd interface" };
-static const char* nameChangedIdTest1 = "nameChangedTest1";
-static const char* nameChangedDescriptionTest1[] =  { "Emitted when the name changes and is sessionless 2nd interface", "Emittiert, wenn der Name andert sessionless 2nd interface", "Naam badalne pe emitte karen aur ye sessionless hai 2nd interface" };
-static const char* testMethodId1 = "testMethod1";
-static const char* testMethodDescription1[] = { "This is first method 2nd interface", "Este es el primer metodo 2nd interface", "Ye pehla method hai 2nd interface" };
-
-static const char* argId = "arg";
-static const char* nameChangedArgDescription[] =  { "This is the new name", "Dies ist der neue Name", "Ye naya naam hai" };
-static const char* propId = "prop";
-static const char* namePropDescription[] =  { "This is the actual name", "Dies ist der eigentliche Name", "Ye asli naam hai" };
-static int translator_status = 0;
 
 class MyTranslator : public Translator {
   public:
@@ -97,64 +75,6 @@ class MyTranslator : public Translator {
     virtual ~MyTranslator() { }
 
     virtual size_t NumTargetLanguages() {
-        return 2;
-    }
-
-    virtual void GetTargetLanguage(size_t index, qcc::String& ret) {
-        ret.assign(tags[index]);
-    }
-
-    virtual const char* Translate(const char* sourceLanguage, const char* targetLanguage, const char* source) {
-        QCC_UNUSED(sourceLanguage);
-        size_t i = 0;
-        if (targetLanguage && (0 == strcmp(targetLanguage, "de"))) {
-            i = 1;
-        }
-
-        if (0 == strcmp(source, objId)) {
-            return objDescription[i];
-        }
-        if (0 == strcmp(source, ifcId)) {
-            return ifcDescription[i];
-        }
-        if (0 == strcmp(source, nameChangedId)) {
-            return nameChangedDescription[i];
-        }
-        if (0 == strcmp(source, nameChangedIdTest)) {
-            return nameChangedDescriptionTest[i];
-        }
-        if (0 == strcmp(source, testMethodId)) {
-            return testMethodDescription[i];
-        }
-        if (0 == strcmp(source, ifcIdTest)) {
-            return ifcDescriptionTest[i];
-        }
-        if (0 == strcmp(source, nameChangedId1)) {
-            return nameChangedDescription1[i];
-        }
-        if (0 == strcmp(source, nameChangedIdTest1)) {
-            return nameChangedDescriptionTest1[i];
-        }
-        if (0 == strcmp(source, testMethodId1)) {
-            return testMethodDescription1[i];
-        }
-        if (0 == strcmp(source, argId)) {
-            return nameChangedArgDescription[i];
-        }
-        if (0 == strcmp(source, propId)) {
-            return namePropDescription[i];
-        }
-        return NULL;
-    }
-
-};
-
-class MyTranslatorTest1 : public Translator {
-  public:
-
-    virtual ~MyTranslatorTest1() { }
-
-    virtual size_t NumTargetLanguages() {
         return 3;
     }
 
@@ -165,117 +85,19 @@ class MyTranslatorTest1 : public Translator {
     virtual const char* Translate(const char* sourceLanguage, const char* targetLanguage, const char* source) {
         QCC_UNUSED(sourceLanguage);
         size_t i = 0;
-        if (targetLanguage && (0 == strcmp(targetLanguage, "hi"))) {
-            i = 2;
-        }
-
-
-        if (0 == strcmp(source, objId)) {
-            return objDescription[i];
-        }
-        if (0 == strcmp(source, ifcId)) {
-            return ifcDescription[i];
-        }
-        if (0 == strcmp(source, nameChangedId)) {
-            return nameChangedDescription[i];
-        }
-        if (0 == strcmp(source, nameChangedIdTest)) {
-            return nameChangedDescriptionTest[i];
-        }
-        if (0 == strcmp(source, testMethodId)) {
-            return testMethodDescription[i];
-        }
-        if (0 == strcmp(source, ifcIdTest)) {
-            return ifcDescriptionTest[i];
-        }
-        if (0 == strcmp(source, nameChangedId1)) {
-            return nameChangedDescription1[i];
-        }
-        if (0 == strcmp(source, nameChangedIdTest1)) {
-            return nameChangedDescriptionTest1[i];
-        }
-        if (0 == strcmp(source, testMethodId1)) {
-            return testMethodDescription1[i];
-        }
-        if (0 == strcmp(source, argId)) {
-            return nameChangedArgDescription[i];
-        }
-        if (0 == strcmp(source, propId)) {
-            return namePropDescription[i];
-        }
-        return NULL;
-    }
-
-};
-
-class MyTranslatorTest2 : public Translator {
-  public:
-
-    virtual ~MyTranslatorTest2() { }
-
-    virtual size_t NumTargetLanguages() {
-        return 3;
-    }
-
-    virtual void GetTargetLanguage(size_t index, qcc::String& ret) {
-        ret.assign(tags[index]);
-    }
-
-    virtual const char* Translate(const char* sourceLanguage, const char* targetLanguage, const char* source) {
-        QCC_UNUSED(sourceLanguage);
-        /* const char* tag = (*targetLanguage == '\0') ? "en" : targetLanguage;
-           size_t i = 0;
-
-           if (0 == strcmp(tag, "en")) {
-             i = 0;
-           } else if (0 == strcmp(tag, "de")) {
-             i = 1;
-           } else if (0 == strcmp(tag, "hi")) {
-             i = 2;
-           } else {
-             return NULL;
-           }*/
-        size_t i = 0;
-        if (targetLanguage && (0 == strcmp(targetLanguage, "de"))) {
-            i = 1;
-        } else if (targetLanguage && (0 == strcmp(targetLanguage, "hi"))) {
-            i = 2;
+        if (targetLanguage) {
+            if (strcmp(targetLanguage, "de") == 0) {
+                i = 1;
+            } else if (strcmp(targetLanguage, "hi") == 0) {
+                i = 2;
+            }
         }
 
         if (0 == strcmp(source, objId)) {
             return objDescription[i];
         }
-        if (0 == strcmp(source, ifcId)) {
-            return ifcDescription[i];
-        }
-        if (0 == strcmp(source, nameChangedId)) {
-            return nameChangedDescription[i];
-        }
-        if (0 == strcmp(source, nameChangedIdTest)) {
-            return nameChangedDescriptionTest[i];
-        }
-        if (0 == strcmp(source, testMethodId)) {
-            return testMethodDescription[i];
-        }
-        if (0 == strcmp(source, ifcIdTest)) {
-            return ifcDescriptionTest[i];
-        }
-        if (0 == strcmp(source, nameChangedId1)) {
-            return nameChangedDescription1[i];
-        }
-        if (0 == strcmp(source, nameChangedIdTest1)) {
-            return nameChangedDescriptionTest1[i];
-        }
-        if (0 == strcmp(source, testMethodId1)) {
-            return testMethodDescription1[i];
-        }
-        if (0 == strcmp(source, argId)) {
-            return nameChangedArgDescription[i];
-        }
-        if (0 == strcmp(source, propId)) {
-            return namePropDescription[i];
-        }
-        return NULL;
+
+        return nullptr;
     }
 
 };
@@ -300,17 +122,25 @@ class BasicSampleObject : public BusObject {
             intf->AddMethod("testMethod", "s", "s", "inStr,outStr");
             intf->AddProperty("name", "s", PROP_ACCESS_RW);
 
-            intf->SetDescriptionLanguage("");
-            intf->SetDescription(ifcId);
-            intf->SetMemberDescription("nameChanged", nameChangedId);
-            intf->SetMemberDescription("nameChangedTest", nameChangedIdTest);
-            intf->SetMemberDescription("testMethod", testMethodId);
-            intf->SetArgDescription("nameChanged", "newName", argId);
-            intf->SetPropertyDescription("name", propId);
+            intf->SetDescriptionForLanguage("This is the first interface", "en");
+            intf->SetDescriptionForLanguage("Dies ist das erste Schnittstelle", "de");
+            intf->SetDescriptionForLanguage("Ye pehla Interface hai", "hi");
+            intf->SetMemberDescriptionForLanguage("nameChanged", "Emitted when the name changes", "en");
+            intf->SetMemberDescriptionForLanguage("nameChanged", "Emittiert, wenn der Name andert", "de");
+            intf->SetMemberDescriptionForLanguage("nameChanged", "Naam badalne pe emitte karen", "hi");
+            intf->SetMemberDescriptionForLanguage("nameChangedTest", "Emitted when the name changes and is sessionless", "en");
+            intf->SetMemberDescriptionForLanguage("nameChangedTest", "Emittiert, wenn der Name andert sessionless", "de");
+            intf->SetMemberDescriptionForLanguage("nameChangedTest", "Naam badalne pe emitte karen aur ye sessionless hai", "hi");
+            intf->SetMemberDescriptionForLanguage("testMethod", "This is the first method", "en");
+            intf->SetMemberDescriptionForLanguage("testMethod", "Dies ist die erste Methode", "de");
+            intf->SetMemberDescriptionForLanguage("testMethod", "Ye pehla method hai", "hi");
+            intf->SetArgDescriptionForLanguage("nameChanged", "newName", "This is the new name", "en");
+            intf->SetArgDescriptionForLanguage("nameChanged", "newName", "Dies ist der neue Name", "de");
+            intf->SetArgDescriptionForLanguage("nameChanged", "newName", "Ye naya naam hai", "hi");
+            intf->SetPropertyDescriptionForLanguage("name", "This is the actual name", "en");
+            intf->SetPropertyDescriptionForLanguage("name", "Dies ist der eigentliche Name", "de");
+            intf->SetPropertyDescriptionForLanguage("name", "Ye asli naam hai", "hi");
 
-            if (translator_status == 3) {
-                intf->SetDescriptionTranslator(&translator);
-            }
             intf->Activate();
         } else {
             printf("Failed to create interface %s\n", INTERFACE_NAME);
@@ -326,43 +156,8 @@ class BasicSampleObject : public BusObject {
             printf("Failed to Add interface: %s", INTERFACE_NAME);
         }
 
-        /* InterfaceDescription* intfTest = NULL;
-           QStatus statusTest = bus.CreateInterface(INTERFACE_NAME_TEST, intfTest);
-           if (statusTest == ER_OK) {
-             intfTest->AddSignal("nameChanged1", "s", "newName", 0);
-             intfTest->AddSignal("nameChangedTest1", "s", "newNameTest", 0);
-             intfTest->AddMethod("testMethod1", "s", "s", "inStr,outStr", 0);
-             intfTest->AddProperty("name1", "s", PROP_ACCESS_RW);
-
-             intfTest->SetDescriptionLanguage("dig");
-             intfTest->SetDescription(ifcIdTest);
-             intfTest->SetMemberDescription("nameChanged1", nameChangedId1, 0);
-             intfTest->SetMemberDescription("nameChangedTest1", nameChangedIdTest1, 1);
-             intfTest->SetMemberDescription("testMethod1", testMethodId1, 1);
-             intfTest->SetArgDescription("nameChanged", "newName", argId);
-             intfTest->SetPropertyDescription("name", propId);
-
-             intfTest->SetDescriptionTranslator(&translator1);
-
-             intfTest->Activate();
-           } else {
-             printf("Failed to create interface %s\n", INTERFACE_NAME_TEST);
-           }
-
-           statusTest = AddInterface(*intfTest);
-
-           if (statusTest == ER_OK) {
-             Register the signal handler 'nameChanged' with the bus
-             nameChangedMember = intfTest->GetMember("nameChanged");
-             QCC_ASSERT(nameChangedMember);
-            } else {
-             printf("Failed to Add interface: %s", INTERFACE_NAME);
-            }*/
-
         SetDescription("", objId);
-        if (translator_status == 2) {
-            SetDescriptionTranslator(&translator);
-        }
+        SetDescriptionTranslator(&translator);
     }
 
     QStatus EmitNameChangedSignal(qcc::String newName)
@@ -416,8 +211,6 @@ class BasicSampleObject : public BusObject {
     qcc::String prop_nameTest;
 
     MyTranslator translator;
-    //MyTranslatorTest1 translator1;
-    //MyTranslatorTest2 translator2;
 };
 
 class MyBusListener : public BusListener, public SessionPortListener {
@@ -533,21 +326,10 @@ void WaitForSigInt(void)
     }
 }
 
-static void usage(void)
-{
-    printf("Usage: signal_service [-h <name>] [-g] [-b] [-i] \n\n");
-    printf("Options:\n");
-    printf("   -h                    = Print this help message\n");
-    printf("   -?                    = Print this help message\n");
-    printf("   -g                    = Set Global level translator\n");
-    printf("   -b                    = Set Bus Attachment level translator\n");
-    printf("   -i                    = Set interface level translator\n");
-    printf("\n");
-}
-
-
 int TestAppMain(int argc, char** argv, char** envArg)
 {
+    QCC_UNUSED(argc);
+    QCC_UNUSED(argv);
     QCC_UNUSED(envArg);
 
     printf("AllJoyn Library version: %s.\n", ajn::GetVersion());
@@ -555,25 +337,6 @@ int TestAppMain(int argc, char** argv, char** envArg)
 
     /* Install SIGINT handler */
     signal(SIGINT, SigIntHandler);
-
-    /* Parse the command line args */
-    for (int i = 1; i < argc; ++i) {
-        if (0 == strcmp("-h", argv[i]) || 0 == strcmp("-?", argv[i])) {
-            usage();
-            exit(0);
-        } else if (0 == strcmp("-g", argv[i])) {
-            translator_status = 1;
-        } else if (0 == strcmp("-b", argv[i])) {
-            translator_status = 2;
-        } else if (0 == strcmp("-i", argv[i])) {
-            translator_status = 3;
-        } else {
-            printf("Unknown option %s\n", argv[i]);
-            usage();
-            exit(1);
-        }
-    }
-
 
     QStatus status = ER_OK;
 
@@ -598,16 +361,6 @@ int TestAppMain(int argc, char** argv, char** envArg)
 
     BasicSampleObject testObj(*s_msgBus, SERVICE_PATH);
 
-    /*
-     * Set Bus Attachment level translator.
-     */
-    MyTranslator busTranslator;
-
-    if (translator_status == 1) {
-        printf("Inside yes\n");
-        // MyTranslator busTranslator;
-        s_msgBus->SetDescriptionTranslator(&busTranslator);
-    }
     if (ER_OK == status) {
         status = RegisterBusObjectAndConnect(&testObj);
     }
