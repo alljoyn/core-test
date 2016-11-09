0054e667425085c529b82d845b6b79e2657ff7d0
Krystian Zlomek <kzlomek@affinegy.com>
2016-11-09 14:09:56 +0100

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: Iaa0fdcd1d3e08b170a041363d3c10e617363dfcd
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- tcl/tc-sc-interaction/AboutTest.cc
+++ tcl/tc-sc-interaction/AboutTest.cc
@@ -1,17 +1,30 @@
 /******************************************************************************
- * Copyright AllSeen Alliance. All rights reserved.
+ *  *    Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+ *    Source Project (AJOSP) Contributors and others.
  *
- *    Permission to use, copy, modify, and/or distribute this software for any
- *    purpose with or without fee is hereby granted, provided that the above
- *    copyright notice and this permission notice appear in all copies.
+ *    SPDX-License-Identifier: Apache-2.0
  *
- *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *    All rights reserved. This program and the accompanying materials are
+ *    made available under the terms of the Apache License, Version 2.0
+ *    which accompanies this distribution, and is available at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+ *    Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for
+ *    any purpose with or without fee is hereby granted, provided that the
+ *    above copyright notice and this permission notice appear in all
+ *    copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ *     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ *     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ *     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ *     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *     PERFORMANCE OF THIS SOFTWARE.
  ******************************************************************************/
 
 #include "ajtcscTestCommon.h"
@@ -453,8 +466,8 @@ TEST_F(AboutTest, TC_Withdrawing_Annnouncement) {
 
     // Wait until the announcement has been received by scBus
     uint16_t remainingWaitTime = WAIT_TIME;
-    while (0 >= testAboutListener->GetNumAnnouncementsReceived() &&
-           0 < remainingWaitTime) {
+    while ((1 > testAboutListener->GetNumAnnouncementsReceived()) &&
+           (0 < remainingWaitTime)) {
         AJ_Sleep(100);
         remainingWaitTime -= 100;
     }
@@ -477,8 +490,8 @@ TEST_F(AboutTest, TC_Withdrawing_Annnouncement) {
 
     // Wait to see if the Announcement is received (it has been cancelled)
     remainingWaitTime = WAIT_TIME;
-    while (0 >= throwAwayAboutListener.GetNumAnnouncementsReceived() &&
-           0 < remainingWaitTime) {
+    while ((0 >= throwAwayAboutListener.GetNumAnnouncementsReceived()) &&
+           (0 < remainingWaitTime)) {
         AJ_Sleep(100);
         remainingWaitTime -= 100;
     }
@@ -491,9 +504,9 @@ TEST_F(AboutTest, TC_Withdrawing_Annnouncement) {
 
     // Wait until both interested parties have received the announcement
     remainingWaitTime = WAIT_TIME;
-    while (1 >= testAboutListener->GetNumAnnouncementsReceived() &&
-           0 >= throwAwayAboutListener.GetNumAnnouncementsReceived() &&
-           0 < remainingWaitTime) {
+    while (((2 > testAboutListener->GetNumAnnouncementsReceived()) ||
+           (1 > throwAwayAboutListener.GetNumAnnouncementsReceived())) &&
+           (0 < remainingWaitTime)) {
         AJ_Sleep(100);
         remainingWaitTime -= 100;
     }
