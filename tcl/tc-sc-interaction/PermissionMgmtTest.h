b9d0dedd5a7e80eb9391233d584820a46ed8ab79
Karol Matuszkiewicz <kmatuszkiewicz@affinegy.com>
2016-11-25 11:27:09 +0100

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: I2de833747805fac30c39f3f3cf2ce8455c2bf4fb
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- tcl/tc-sc-interaction/PermissionMgmtTest.h
+++ tcl/tc-sc-interaction/PermissionMgmtTest.h
@@ -4,19 +4,32 @@
  */
 
 /******************************************************************************
- * Copyright AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2016 Open Connectivity Foundation (OCF) and AllJoyn Open
+ *    Source Project (AJOSP) Contributors and others.
  *
- *    Permission to use, copy, modify, and/or distribute this software for any
- *    purpose with or without fee is hereby granted, provided that the above
- *    copyright notice and this permission notice appear in all copies.
+ *    SPDX-License-Identifier: Apache-2.0
  *
- *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *    All rights reserved. This program and the accompanying materials are
+ *    made available under the terms of the Apache License, Version 2.0
+ *    which accompanies this distribution, and is available at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Copyright 2016 Open Connectivity Foundation and Contributors to
+ *    AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for
+ *    any purpose with or without fee is hereby granted, provided that the
+ *    above copyright notice and this permission notice appear in all
+ *    copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ *     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ *     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ *     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ *     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *     PERFORMANCE OF THIS SOFTWARE.
  ******************************************************************************/
 
 #include <qcc/platform.h>
@@ -70,6 +83,10 @@ class BasePermissionMgmtTest : public testing::Test, public BusObject {
     static const char* NOTIFY_INTERFACE_NAME;
     static const char* ONOFF_IFC_NAME;
     static const char* TV_IFC_NAME;
+    static const char* ADMIN_BUS_NAME;
+    static const char* SERVICE_BUS_NAME;
+    static const char* CONSUMER_BUS_NAME;
+    static const char* RC_BUS_NAME;
 
     class ECDHEKeyXListener : public AuthListener {
       public:
@@ -163,20 +180,20 @@ class BasePermissionMgmtTest : public testing::Test, public BusObject {
     };
 
     BasePermissionMgmtTest(const char* path) : BusObject(path),
-        adminBus("PermissionMgmtTestAdmin", false),
-        serviceBus("PermissionMgmtTestService", false),
-        consumerBus("PermissionMgmtTestConsumer", false),
-        remoteControlBus("PermissionMgmtTestRemoteControl", false),
+        adminBus(ADMIN_BUS_NAME, false),
+        serviceBus(SERVICE_BUS_NAME, false),
+        consumerBus(CONSUMER_BUS_NAME, false),
+        remoteControlBus(RC_BUS_NAME, false),
         adminAdminGroupGUID("00112233445566778899AABBCCDDEEFF"),
         consumerAdminGroupGUID("AABBCCDDEEFF00112233445566778899"),
         serviceGUID(),
         consumerGUID(),
         remoteControlGUID(),
         status(ER_OK),
-        serviceKeyListener(NULL),
-        adminKeyListener(NULL),
-        consumerKeyListener(NULL),
-        remoteControlKeyListener(NULL),
+        serviceKeyListener(nullptr),
+        adminKeyListener(nullptr),
+        consumerKeyListener(nullptr),
+        remoteControlKeyListener(nullptr),
         canTestStateSignalReception(false),
         currentTVChannel(1),
         volume(1),
@@ -184,6 +201,10 @@ class BasePermissionMgmtTest : public testing::Test, public BusObject {
         testASL(),
         testPCL()
     {
+        adminBus.DeleteDefaultKeyStore(ADMIN_BUS_NAME);
+        serviceBus.DeleteDefaultKeyStore(SERVICE_BUS_NAME);
+        consumerBus.DeleteDefaultKeyStore(CONSUMER_BUS_NAME);
+        remoteControlBus.DeleteDefaultKeyStore(RC_BUS_NAME);
     }
 
     virtual void SetUp();
